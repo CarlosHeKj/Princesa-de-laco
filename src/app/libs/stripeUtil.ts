da38a56eaf7401f2966f2b55f01bfff6247a0296
CarlosHeKj <carloslouise2015@gmail.com>
2025-05-30 01:08:30 -0300

    corrigindo preço

--- src/app/libs/stripeUtil.ts
+++ src/app/libs/stripeUtil.ts
@@ -10,14 +10,14 @@ export async function getAllProducts(): Promise<ProductType[]> {
   const priceList = await stripe.prices.list({ active: true });
 
   const allProducts: ProductType[] = productList.data.map((product) => {
-    const price = priceList.data.find((p) => p.product === product.id);
+     const price = priceList.data.find((p) => p.product === product.id && p.unit_amount != null);
 
     return {
       id: product.id,
       name: product.name,
       description: product.description ?? "",
       image: product.images?.[0] ?? "",
-      price: price?.unit_amount ? price.unit_amount / 100 : 0,
+      price: price?.unit_amount ? price.unit_amount / 1: 0,
       currency: price?.currency ?? "BRL",
       category: product.metadata.category ?? "Sem categoria",
     };
