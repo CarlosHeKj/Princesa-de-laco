a4cbe0b7ffcc8350dd74e127b938003b08f80b9e
CarlosHeKj <carloslouise2015@gmail.com>
2025-05-30 00:54:53 -0300

    alterando para const

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
-import { getProducts } from "@/app/libs/stripeUtil";
+import { getAllProducts } from "@/app/libs/stripeUtil";
 
 export async function GET() {
   try {
-    const products = await getProducts();
+    const products = await getAllProducts();
     return NextResponse.json(products);
   } catch (error) {
     console.error("Erro ao processar a requisição:", error);
